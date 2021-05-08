7843b446587fcb8ffeb81f3477f20e4dcb535eab
Studioname <83342697+Studioname@users.noreply.github.com>
2021-05-08 16:04:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ import random
 import art
 import replit
 
-turn = True
-computer_turn = True
-
 playing = True
 while playing == True:
-  play = input('Would you like to play a game of Blackjack? Type "y" to play or "n" to quit\n')
+  
+  strings = {
+    
+    "play" : input('  Would you like to play a game of Blackjack? Type "y" to play or "n" to quit\n'),
+    "both_lose" : "  Both your scores were over 21. You both lose!",
+    "player_bust" : "  Bust! Computer wins",
+    "computer_bust" : "  Bust! Player wins",
+    "player_wins" : "  Congratulations, you win!",
+    "computer_wins" : "  Unlucky, computer wins!",
+    "draw" : "  Both have the same score. Draw!"
+  }
+  
+  print(strings["play"])
 
   #quits app if n pressed
-  if play == "y":
+  if strings["play"] == "y":
     replit.clear()
-  elif play == "n":
+
+  else:
     playing = False
     print("Goodbye!")
     quit()
@@ -46,65 +56,81 @@ while playing == True:
   
   #player turn
 
-  
   draw_card(player_hand)
   draw_card(player_hand)
-  while turn == True:
+  def player_turn():
     player_cards = "  Your cards are : "
     for card in player_hand:
       player_cards += str(card) + " "
-    player_score_is = (f" and your score is {get_score(player_hand)}")
+    player_score_is = (f"and your score is {get_score(player_hand)}")
     print(player_cards + player_score_is)
 
     score = get_score(player_hand)
-    if score > 21:
-      if cards[0] in player_hand:
+    turn = True
+    while turn == True:
+      score = get_score(player_hand)
+      if cards[0] in player_hand and score > 21:
         index = player_hand.index(11)
         player_hand[index] = 1
         print(" Converting an ace from 11 to 1!")
-      else: 
-        turn = False
-
-    draw = input('  Would you like to draw another card? Type "y" to draw or "n" to pass\n')
-    if draw == "y":
-      draw_card(player_hand)
-    else:
-      turn = False
+        continue
+      if score < 21:
+        draw = input('  Would you like to draw another card? Type "y" to draw or any other key to pass\n')
+        if draw == "y":
+          draw_card(player_hand)
+          player_turn()
+        else:
+          break
+      break
   
   #computer turn
 
-  while computer_turn == True:
-    if get_score(computer_hand) > 16:
-      if score > 21:
-        if cards[0] in computer_hand:
+  def computer_turn():
+
+    score = get_score(computer_hand)
+
+    turn = True
+    while turn == True:
+      while score <= 16: 
+        draw_card(computer_hand)
+
+        if cards[0] in computer_hand and score > 21:
           index = computer_hand.index(11)
           computer_hand[index] = 1
           print(" Converting computer's ace from 11 to 1!")
-      else: 
-        computer_turn = False
+        score = get_score(computer_hand)
         computer_cards = "  The computer's cards are : "
         for card in computer_hand:
           computer_cards += str(card) + " "
-          print(computer_cards)
-    else:
-      draw_card(computer_hand)
+        computer_score_is = (f"and the computer's score is {score}")
+        print(computer_cards + computer_score_is)
+
+      else:
+        break
+      break
 
   #compare scores
 
-  computer_score = get_score(computer_hand)
-  player_score = get_score(player_hand)
+  def compare_scores():
+    computer_score = get_score(computer_hand)
+    player_score = get_score(player_hand)
 
-  print(f"  Your final score is {player_score}")
-  print(f"  The computer's final score is {computer_score}")
-  if player_score > 21 and computer_score > 21:
-    print(" Both your scores were over 21. You both lose!")
-  elif player_score > 21:
-    print("  Bust! Computer wins")
-  elif computer_score > 21:
-    print(" Bust! Player wins")
-  elif player_score > computer_score:
-    print(" Congratulations, you win!")
-  elif computer_score > player_score:
-    print("  Unlucky, computer wins!")
-  elif computer_score == player_score:
-    print(" Both have the same score. Draw!")
+    print(f"  Your final score is {player_score}")
+    print(f"  The computer's final score is {computer_score}")
+    if player_score > 21 and computer_score > 21:
+      print(strings["both_lose"])
+    elif player_score > 21:
+      print(strings["player_bust"])
+    elif computer_score > 21:
+      print(strings["computer_bust"])
+    elif player_score > computer_score:
+      print(strings["player_wins"])
+    elif computer_score > player_score:
+      print(strings["computer_wins"])
+    elif computer_score == player_score:
+      print(strings["draw"])
+
+  player_turn()
+  computer_turn()
+  compare_scores()
+  
